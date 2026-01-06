2498553cefb328674aeba590cfc073511f6cc1d3
Gustavo Esteban <ext.gustavo.jorge@lmmobilidade.com.br>
2026-01-06 19:48:43 -0300

    Refactor: Switch to database-only storage and remove mock data

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,6 @@ import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { conLicitacaoStorage } from "./conlicitacao-storage";
-import { databaseStorage } from "./database-storage";
 import { syncService } from "./sync-service";
 import { loginSchema, registerSchema, forgotPasswordSchema } from "@shared/schema";
 import bcrypt from "bcrypt";
